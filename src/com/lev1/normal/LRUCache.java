c4245db542f65d911cb604d0cd4e6536fc98dcc2
Lev1_MT <14108426@hdu.edu.cn>
2019-11-07 20:01:21 +0800

    LRU Cache

--- src/com/lev1/normal/LRUCache.java
+++ src/com/lev1/normal/LRUCache.java
@@ -1,5 +1,6 @@
 package com.lev1.normal;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -12,12 +13,12 @@ public class LRUCache extends LinkedHashMap<Integer, Integer> {
         this.capacity = capacity;
     }
 
-    public int get(int key){
-        return super.getOrDefault(key,-1);
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
     }
 
-    public void put(int key, int value){
-        super.put(key,value);
+    public void put(int key, int value) {
+        super.put(key, value);
     }
 
     @Override
@@ -27,14 +28,73 @@ public class LRUCache extends LinkedHashMap<Integer, Integer> {
 
     public static void main(String[] args) {
         LRUCache cache = new LRUCache(2);
-        cache.put(1,1);
-        cache.put(2,2);
+        cache.put(1, 1);
+        cache.put(2, 2);
         System.out.println(cache.get(1));
-        cache.put(3,3);
+        cache.put(3, 3);
         System.out.println(cache.get(2));
-        cache.put(4,4);
+        cache.put(4, 4);
         System.out.println(cache.get(1));
         System.out.println(cache.get(3));
         System.out.println(cache.get(4));
+    }
+}
+
+
+class LRUCache2 {
+    private DLinkedNode head, tail;
+    private int size, capacity;
+    private Map<Integer, DLinkedNode> map = new HashMap<>();
+
+    class DLinkedNode {
+        public int key, value;
+        public DLinkedNode prev, next;
+
+        public DLinkedNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public DLinkedNode() {
+        }
+    }
+
+    // 向链表中插入节点，每次插入都放在head节点后面
+    private void addNode(DLinkedNode node) {
+        node.prev = head;
+        node.next = head.next;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    // 删除指定节点
+    private void removeNode(DLinkedNode node) {
+        DLinkedNode prev = node.prev;
+        DLinkedNode next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    // 将节点移动到双向链表头部
+    private void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addNode(node);
+    }
+
+    private DLinkedNode popTail() {
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    public LRUCache2(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode newNode = new DLinkedNode(key, value);
+        addNode(newNode);
     }
 }
