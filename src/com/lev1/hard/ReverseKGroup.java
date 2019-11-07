aee9f1c22ffb08da5260540778b75c22e46fed31
王桐 <wb-wt589765@antfin.com>
2019-11-07 15:14:56 +0800

    24. 两两交换链表中的节点

--- src/com/lev1/hard/ReverseKGroup.java
+++ src/com/lev1/hard/ReverseKGroup.java
@@ -1,13 +1,8 @@
-package com.lev1.medium;
+package com.lev1.hard;
 
 /**
- * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
- * k 是一个正整数，它的值小于或等于链表的长度。
- * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
- * 示例 :
- * 给定这个链表：1->2->3->4->5
- * 当 k = 2 时，应当返回: 2->1->4->3->5
- * 当 k = 3 时，应当返回: 3->2->1->4->5
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。 k 是一个正整数，它的值小于或等于链表的长度。 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。 示例 : 给定这个链表：1->2->3->4->5 当 k = 2 时，应当返回:
+ * 2->1->4->3->5 当 k = 3 时，应当返回: 3->2->1->4->5
  */
 public class ReverseKGroup {
 
@@ -26,19 +21,20 @@ public class ReverseKGroup {
 
     /**
      * 按照K步长翻转单链表
+     *
      * @param head 需要翻转的单链表
-     * @param k 步长
+     * @param k    步长
      * @return 翻转结果
      */
     public static ListNode reverseKGroup(ListNode head, int k) {
         ListNode temp = head;
 
         // 截取单链表
-        for (int i = 1; i < k && temp!= null; i++) {
+        for (int i = 1; i < k && temp != null; i++) {
             temp = temp.next;
         }
         // 当剩下的节点数不足步长数时，不进行翻转
-        if (temp == null) return head;
+        if (temp == null) { return head; }
 
         // 保存剩余待翻转节点
         ListNode t2 = temp.next;
@@ -54,18 +50,36 @@ public class ReverseKGroup {
 
     /**
      * 单链表反转
+     *
      * @param head 需要反转的链表
      * @return 反转之后的链表
      */
-    public static ListNode reverse(ListNode head){
+    public static ListNode reverse(ListNode head) {
         ListNode temp = head;
         ListNode target = null;
-        while (temp != null){
+        while (temp != null) {
             ListNode cur = temp.next;
             temp.next = target;
             target = temp;
             temp = cur;
         }
         return target;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "val=" + val +
+                "," + next +
+                '}';
     }
 }
---
+++ src/com/lev1/medium/SwapPairs.java
@@ -0,0 +1,60 @@
+package com.lev1.medium;
+
+/**
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 示例: 给定 1->2->3->4, 你应该返回 2->1->4->3.
+ */
+public class SwapPairs {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        System.out.println(swapPairs(node1));
+
+        System.out.println(swapPairs2(node2));
+    }
+
+    /**
+     * 递归实现链表两两翻转
+     *
+     * @param head 需要翻转的链表
+     * @return 翻转后的链表
+     */
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = head.next;
+        head.next = swapPairs(temp.next);
+        temp.next = head;
+        return temp;
+    }
+
+    /**
+     * 非递归实现链表两两翻转
+     *
+     * @param head 需要翻转的链表
+     * @return 翻转后的链表
+     */
+    public static ListNode swapPairs2(ListNode head) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = pre;
+
+        while (temp.next != null && temp.next.next != null) {
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+        return pre.next;
+    }
+}
