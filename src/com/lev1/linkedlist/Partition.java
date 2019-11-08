9c3d1e361b34933824b77d75b6439dca2402dd99
王桐 <wb-wt589765@antfin.com>
2019-11-08 11:52:16 +0800

    86. 分隔链表

---
+++ src/com/lev1/linkedlist/Partition.java
@@ -0,0 +1,45 @@
+package com.lev1.linkedlist;
+
+/**
+ * 给定一个链表和一个特定值 x，对链表进行分隔，使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+ * 你应当保留两个分区中每个节点的初始相对位置。
+ * 示例:
+ * 输入: head = 1->4->3->2->5->2, x = 3
+ * 输出: 1->2->2->4->3->5
+ */
+public class Partition {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(4);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(2);
+        ListNode node5 = new ListNode(2);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        System.out.println(partition(node1, 3));
+    }
+
+    public static ListNode partition(ListNode head, int x) {
+        ListNode beforeHead = new ListNode(0);
+        ListNode before = beforeHead;
+        ListNode afterHead = new ListNode(0);
+        ListNode after = afterHead;
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+            head = head.next;
+        }
+        // 这一步是为了删除 after指针后的多余节点(链表尾部小于x的节点会跟在after后面)
+        after.next = null;
+        before.next = afterHead.next;
+        return beforeHead.next;
+    }
+}
