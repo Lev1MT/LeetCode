e327412e2bbdbe9f332efca7eac33fb68d05ca2c
王桐 <wb-wt589765@antfin.com>
2019-11-26 10:46:58 +0800

    92. 反转链表 II

--- src/com/lev1/linkedlist/ReverseBetween.java
+++ src/com/lev1/linkedlist/ReverseBetween.java
@@ -1,10 +1,16 @@
 package com.lev1.linkedlist;
 
 /**
- *
+ * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+ * 说明:
+ * 1 ≤ m ≤ n ≤ 链表长度。
+ * 示例:
+ * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
+ * 输出: 1->4->3->2->5->NULL
  */
 public class ReverseBetween {
 
+    // 用于保存n之后的链表节点
     public static ListNode successor = null;
 
     public static void main(String[] args) {
@@ -19,6 +25,7 @@ public class ReverseBetween {
         node4.next = node5;
         System.out.println(reverseN(node1, 3));
     }
+
 
     public static ListNode reverseBetween(ListNode head, int m, int n) {
         if (m == 1) {
